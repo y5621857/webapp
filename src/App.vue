0991ab18cbd5aef1aebfb8d8ba43d681f46a1961
y5621857 <229655153@qq.com>
2017-11-29 14:22:47 +0800

    Add
    Add

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div id="wrapper">
+      <!--左侧导航-->
+      <left-slide-menu></left-slide-menu>
+      <div id="page-wrapper" class="gray-bg dashbard-1">
+        <!--顶部导航-->
+        <right-top-nav></right-top-nav>
+        <router-view/>
+        <!--页脚-->
+        <footer-bar></footer-bar>
+      </div>
+      
+      <!--聊天控件-->
+      <small-chat></small-chat>
+      <!--右侧弹出框-->
+      <right-slide-bar></right-slide-bar>
+    </div>
+  </div>
+</template>
+
+<script>
+  import LeftSlideMenu from '@/components/base/LeftSlideMenu'
+  import RightTopNav from '@/components/base/RightTopNav'
+  import RightSlideBar from '@/components/base/RightSlideBar'
+  import SmallChat from '@/components/base/SmallChat'
+  import FooterBar from '@/components/base/FooterBar'
+  
+  export default {
+    name: 'app',
+    components: {
+      LeftSlideMenu,
+      RightTopNav,
+      RightSlideBar,
+      SmallChat,
+      FooterBar
+    },
+    mounted(){
+      toastr.options = {
+        closeButton: true,
+        progressBar: true,
+        showMethod: 'slideDown',
+        timeOut: 4000
+      };
+    },
+    
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/base/FooterBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer">
+    <div class="pull-right">
+      10GB of <strong>250GB</strong> Free.
+    </div>
+    <div>
+      <strong>Copyright</strong> Example Company &copy; 2017-2018
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FooterBar',
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
